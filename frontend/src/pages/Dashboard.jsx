7f2c7a27da83cbd5d84f1e1b5fd0ae74ba35a7a6
SuzyQ <153325350+SuzyQ-2117@users.noreply.github.com>
2024-10-14 17:44:26 +0100

    Added grid layout

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,7 +1,46 @@
 import React from 'react';
+import Welcome from '../widgets/Welcome';
 
 const Dashboard = () => {
-  return <div>Dashboard Page</div>;
+  return (
+    <div>
+      <div className="intro-container">
+
+        <div className="welcome-widget">
+          <Welcome />
+        </div>
+
+        <div className="calendar-widget">
+
+        </div>
+
+        <div className="pieChart-widget">
+
+        </div>
+
+        <div className="transactions-widget">
+
+        </div>
+
+        <div className="savings-widget">
+
+        </div>
+
+        <div className="totalSpend-widget">
+
+        </div>
+
+        <div className="topCategories-widget">
+
+        </div>
+
+        <div className="creditCard-widget">
+
+        </div>
+      </div>
+    </div>
+  )
+
 };
 
 export default Dashboard;
